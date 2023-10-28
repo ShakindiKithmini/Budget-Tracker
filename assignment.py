7c0d3850dcb0807f00554851cf477e12927dd7a5
Shakindi Kithmini <shakindikithmini@gmail.com>
2023-10-28 16:43:26 +0530

    Initial commit

---
+++ assignment.py
@@ -0,0 +1,78 @@
+class BudgetTracker:
+    def __init__(self):
+        self.monthly_income = 0
+        self.expenses = []
+
+    # Add sepcified amount to the user's monthly income
+    def add_monthly_income(self, amount):
+        self.monthly_income += amount
+
+    # Add new expenses to the expense list
+    def add_expense(self, date, category, amount):
+        self.expenses.append({
+            "date": date,
+            "category": category,
+            "amount": amount
+        })
+
+    # Prints a list of all of the user's expenses
+    def view_expense_list(self):
+        print("Expense List:")
+        for expense in self.expenses:
+            print(f"{expense['date']} | {expense['category']} | {expense['amount']}")
+
+    # Removes an expense from the user's expense list at the specified index.
+    def remove_expense(self, index):
+        self.expenses.pop(index)
+
+    # Calculates the user's remaining income by subtracting the total amount of their expenses from their monthly income
+    def calculate_remaining_income(self):
+        total_expenses = 0
+        for expense in self.expenses:
+            total_expenses += expense["amount"]
+        remaining_income = self.monthly_income - total_expenses
+        return remaining_income
+
+    # Prints a thank you message and exits the program.
+    def exit(self):
+        print("Thank you!")
+
+def main():
+    budget_tracker = BudgetTracker()
+
+    # Displays a menu of options to the user to enter their choice
+    while True:
+        print("Budget Tracker Menu:")
+        print("1. Add monthly income")
+        print("2. Add expense")
+        print("3. View expense list")
+        print("4. Remove expense")
+        print("5. Calculate remaining income")
+        print("6. Exit")
+
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            amount = float(input("Enter your monthly income:LKR. "))
+            budget_tracker.add_monthly_income(amount)
+        elif choice == 2:
+            date = (input("Enter the date of the expense: "))
+            category = input("Enter the category of the expense: ")
+            amount = float(input("Enter the amount of the expense:LKR. "))
+            budget_tracker.add_expense(date, category, amount)
+        elif choice == 3:
+            budget_tracker.view_expense_list()
+        elif choice == 4:
+            index = int(input("Enter the index of the expense to remove: "))
+            budget_tracker.remove_expense(index)
+        elif choice == 5:
+            remaining_income = budget_tracker.calculate_remaining_income()
+            print("Your remaining income is:LKR.", remaining_income)
+        elif choice == 6:
+            budget_tracker.exit()
+            break
+        else:
+            print("Invalid choice!")
+
+if __name__ == "__main__":
+    main()
